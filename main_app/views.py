5b019f6805a749edfc80a020c5aef3485449d93f
Zoneam <zoneam@gmail.com>
2022-05-21 12:17:19 -0700

    added requirements installed boto3 added imports in views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import os
+import uuid
+import boto3
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from .models import Equipment, Bookings, Photo, Transactions, Customer
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
