1c954272982b28f7a5c9d8f999da5efa3014b011
lauren <laurencrosariol@yahoo.com>
2022-05-20 20:19:50 -0700

    some html and some work on paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -18,11 +18,33 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    #home page
     path('bookings/', views.bookings, name='bookings'),
-    path('customers/', views.customers, name='customers'),
-    path('gear/', views.gear, name='gear'),
+    #bookings page
+
+
+
+
+
+
+    
+    path('customer/', views.CustomerList.as.view(), name='customer_index'),
+    #customer page
+    path('customer/<intA:pk>/', views.CustomerDetail.as_view(), name='customer_detail'),
+    #view customer detail
+    path('customer/create/', views.CustomerCreate, name='customer_create'),
+    #create a new customer
+    path('customer/<int:pk>/update/', views.CustomerUpdate.as_view(), name='customer_update'),
+    #update a customer
+    path('customer/<int:pk>/delete/', views.CustomerDelete.as_view(), name='customer_delete'),
+    #delete a customer
+    path('equiptment/', views.equiptment, name='equiptment'),
+    #equiptment page
     path('portfolio/', views.portfolio, name='portfolio'),
+    #portfolio page
     path('transactions/', views.transactions, name='transactions'),
+    #list of transactions may be seperate page or added to customer page...
     path('profile/', views.profile, name='profile'),
+    #user profile page
 
 ]
