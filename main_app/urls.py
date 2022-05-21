de82b9de8de74ff20f54cf1d92a5b34cb1f13b3b
Lauren Crosariol <101290054+lcrosariol@users.noreply.github.com>
2022-05-20 20:22:40 -0700

    Paths (#4)
    * added some html and paths
    
    * added more html
    
    * some html and some work on paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,50 @@
-from django.urls import path
+"""photofy URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    #home page
+    path('bookings/', views.bookings, name='bookings'),
+    #bookings page
+
+
+
+
+
+
+    
+    path('customer/', views.CustomerList.as.view(), name='customer_index'),
+    #customer page
+    path('customer/<intA:pk>/', views.CustomerDetail.as_view(), name='customer_detail'),
+    #view customer detail
+    path('customer/create/', views.CustomerCreate, name='customer_create'),
+    #create a new customer
+    path('customer/<int:pk>/update/', views.CustomerUpdate.as_view(), name='customer_update'),
+    #update a customer
+    path('customer/<int:pk>/delete/', views.CustomerDelete.as_view(), name='customer_delete'),
+    #delete a customer
+    path('equiptment/', views.equiptment, name='equiptment'),
+    #equiptment page
+    path('portfolio/', views.portfolio, name='portfolio'),
+    #portfolio page
+    path('transactions/', views.transactions, name='transactions'),
+    #list of transactions may be seperate page or added to customer page...
+    path('profile/', views.profile, name='profile'),
+    #user profile page
+
 ]
