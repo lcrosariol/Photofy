d75b18030b4b78c9c37ff03f48bd0a9e8f2ef845
lauren <laurencrosariol@yahoo.com>
2022-05-23 11:18:59 -0700

    fixed logout issue

--- photify/settings.py
+++ photify/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = '/bookings'
-LOGOUT_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
