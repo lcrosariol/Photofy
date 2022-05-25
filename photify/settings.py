15f843b9ee252344afea23489121a35e503c5f42
lauren <laurencrosariol@yahoo.com>
2022-05-25 01:53:01 -0700

    fun video option for future adaptation

--- photify/settings.py
+++ photify/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'photify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'photofy',
-        'USER': 'myappuser',
-        'PASSWORD' : 'mypass',
-        'HOST': 'localhost',
-        'PORT' : 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
